116b894ea34cdf98d9f6b51171543419b40c7d9d
Tiago Dinis <tiagodinis33@proton.me>
2023-12-22 23:20:01 +0000

    wire more stuff into nix-colors

--- homes/x86_64-linux/tiago@nixos-lenovo/default.nix
+++ homes/x86_64-linux/tiago@nixos-lenovo/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [ inputs.nix-colors.homeManagerModules.default ];
   # -------------------------- Nix Colors --------------------------
-  colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
+  colorScheme = inputs.nix-colors.colorSchemes.dracula;
   # ---------------------------- Dunst -----------------------------
   services.dunst = {
     enable = true;
@@ -40,7 +40,7 @@
 
     settings = {
       exec = [ "eww --restart open bar" ];
-      exec-once = ["hypr"];
+      exec-once = [ "hypr" ];
       # Set mod key to super
       "$mod" = "SUPER";
       bindm = [ "$mod,mouse:272,movewindow" "$mod,mouse:273,resizewindow" ];
@@ -116,9 +116,7 @@
       ];
   };
   # --------------------------------- Lazygit -----------------------------------
-  programs.lazygit = {
-    enable = true;
-  };
+  programs.lazygit = { enable = true; };
 
   # -------------------------------- Alacritty ----------------------------------
   programs.alacritty = {
@@ -190,10 +188,190 @@
     };
   };
   # -------------------------------- Nushell ----------------------------------
-  programs.nushell.enable = true;
+  programs.nushell = {
+    enable = true;
+    shellAliases = {
+      lg = "lazygit";
+    };
+  };
   # --------------------------------- Helix -----------------------------------
-  programs.helix.enable = true;
-  programs.helix.defaultEditor = true;
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings.theme = "base16nix";
+    themes.base16nix = {
+      "attributes" = "base09";
+      "comment" = {
+        fg = "base03";
+        modifiers = [ "italic" ];
+      };
+      "constant" = "base09";
+      "constant.character.escape" = "base0C";
+      "constant.numeric" = "base09";
+      "constructor" = "base0D";
+      "debug" = "base03";
+      "diagnostic" = { modifiers = [ "underlined" ]; };
+      "diff.delta" = "base09";
+      "diff.minus" = "base08";
+      "diff.plus" = "base0B";
+      "error" = "base08";
+      "function" = "base0D";
+      "hint" = "base03";
+      "info" = "base0D";
+      "keyword" = "base0E";
+      "label" = "base0E";
+      "namespace" = "base0E";
+      "operator" = "base05";
+      "special" = "base0D";
+      "string" = "base0B";
+      "type" = "base0A";
+      "variable" = "base08";
+      "variable.other.member" = "base0B";
+      "warning" = "base09";
+
+      "markup.bold" = {
+        fg = "base0A";
+        modifiers = [ "bold" ];
+      };
+      "markup.heading" = "base0D";
+      "markup.italic" = {
+        fg = "base0E";
+        modifiers = [ "italic" ];
+      };
+      "markup.link.text" = "base08";
+      "markup.link.url" = {
+        fg = "base09";
+        modifiers = [ "underlined" ];
+      };
+      "markup.list" = "base08";
+      "markup.quote" = "base0C";
+      "markup.raw" = "base0B";
+      "markup.strikethrough" = { modifiers = [ "crossed_out" ]; };
+
+      "diagnostic.hint" = { underline = { style = "curl"; }; };
+      "diagnostic.info" = { underline = { style = "curl"; }; };
+      "diagnostic.warning" = { underline = { style = "curl"; }; };
+      "diagnostic.error" = { underline = { style = "curl"; }; };
+
+      "ui.background" = { bg = "base00"; };
+      "ui.bufferline.active" = {
+        fg = "base00";
+        bg = "base03";
+        modifiers = [ "bold" ];
+      };
+      "ui.bufferline" = {
+        fg = "base04";
+        bg = "base00";
+      };
+      "ui.cursor" = {
+        fg = "base0A";
+        modifiers = [ "reversed" ];
+      };
+      "ui.cursor.insert" = {
+        fg = "base0A";
+        modifiers = [ "revsered" ];
+      };
+      "ui.cursorline.primary" = {
+        fg = "base05";
+        bg = "base01";
+      };
+      "ui.cursor.match" = {
+        fg = "base0A";
+        modifiers = [ "reversed" ];
+      };
+      "ui.cursor.select" = {
+        fg = "base0A";
+        modifiers = [ "reversed" ];
+      };
+      "ui.gutter" = { bg = "base00"; };
+      "ui.help" = {
+        fg = "base06";
+        bg = "base01";
+      };
+      "ui.linenr" = {
+        fg = "base03";
+        bg = "base00";
+      };
+      "ui.linenr.selected" = {
+        fg = "base04";
+        bg = "base01";
+        modifiers = [ "bold" ];
+      };
+      "ui.menu" = {
+        fg = "base05";
+        bg = "base01";
+      };
+      "ui.menu.scroll" = {
+        fg = "base03";
+        bg = "base01";
+      };
+      "ui.menu.selected" = {
+        fg = "base01";
+        bg = "base04";
+      };
+      "ui.popup" = { bg = "base01"; };
+      "ui.selection" = { bg = "base02"; };
+      "ui.selection.primary" = { bg = "base02"; };
+      "ui.statusline" = {
+        fg = "base04";
+        bg = "base01";
+      };
+      "ui.statusline.inactive" = {
+        bg = "base01";
+        fg = "base03";
+      };
+      "ui.statusline.insert" = {
+        fg = "base00";
+        bg = "base0B";
+      };
+      "ui.statusline.normal" = {
+        fg = "base00";
+        bg = "base03";
+      };
+      "ui.statusline.select" = {
+        fg = "base00";
+        bg = "base0F";
+      };
+      "ui.text" = "base05";
+      "ui.text.focus" = "base05";
+      "ui.virtual.indent-guide" = { fg = "base03"; };
+      "ui.virtual.inlay-hint" = { fg = "base01"; };
+      "ui.virtual.ruler" = { bg = "base01"; };
+      "ui.window" = { bg = "base01"; };
+      palette = {
+        base00 = "#${config.colorScheme.colors.base00}"; # Default Background
+        base01 =
+          "#${config.colorScheme.colors.base01}"; # Lighter Background (Used for status bars, line number and folding marks)
+        base02 = "#${config.colorScheme.colors.base02}"; # Selection Background
+        base03 =
+          "#${config.colorScheme.colors.base03}"; # Comments, Invisibles, Line Highlighting
+        base04 =
+          "#${config.colorScheme.colors.base04}"; # Dark Foreground (Used for status bars)
+        base05 =
+          "#${config.colorScheme.colors.base05}"; # Default Foreground, Caret, Delimiters, Operators
+        base06 =
+          "#${config.colorScheme.colors.base06}"; # Light Foreground (Not often used)
+        base07 =
+          "#${config.colorScheme.colors.base07}"; # Light Background (Not often used)
+        base08 =
+          "#${config.colorScheme.colors.base08}"; # Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+        base09 =
+          "#${config.colorScheme.colors.base09}"; # Integers, Boolean, Constants, XML Attributes, Markup Link Url
+        base0A =
+          "#${config.colorScheme.colors.base0A}"; # Classes, Markup Bold, Search Text Background
+        base0B =
+          "#${config.colorScheme.colors.base0B}"; # Strings, Inherited Class, Markup Code, Diff Inserted
+        base0C =
+          "#${config.colorScheme.colors.base0C}"; # Support, Regular Expressions, Escape Characters, Markup Quotes
+        base0D =
+          "#${config.colorScheme.colors.base0D}"; # Functions, Methods, Attribute IDs, Headings
+        base0E =
+          "#${config.colorScheme.colors.base0E}"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
+        base0F =
+          "#${config.colorScheme.colors.base0F}"; # Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+      };
+    };
+  };
   # ---------------------------------- Eww ------------------------------------
   programs.eww.enable = true;
   programs.eww.package = pkgs.eww-wayland;
