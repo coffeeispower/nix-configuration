039c022f78bd7633f7ba4b9f74579263039e216d
Tiago Dinis <tiagodinis33@proton.me>
2024-02-14 00:36:29 +0000

    eww: improve the eww bar A LOT
    another orgasm moment :kreygasm:

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -15,9 +15,9 @@
         rounding = 10;
         blur = {
           enabled = true;
-          size = 3;
-          passes = 1;
-          vibrancy = 0.1696;
+          size = 5;
+          passes = 2;
+          vibrancy = 0.5;
         };
       };
       exec-once = [ "${pkgs.fcitx5}/bin/fcitx5 -d" ];
@@ -25,7 +25,6 @@
       "$mod" = "SUPER";
 
       # Move apps with the mouse
-      bindm = [ "$mod,mouse:272,movewindow" "$mod,mouse:273,resizewindow" ];
 
       windowrule = ["tile,title:^(Minecraft)(.*)$"];
       # Enable workspace swipe
@@ -36,16 +35,12 @@
         ", XF86AudioRaiseVolume, exec, ${pkgs.pamixer}/bin/pamixer --increase 5"
       ];
       bind = [
-        ''$mod, S, exec, hyprctl keyword bind ", Escape, exec, eww close shutdown"''
-        ''$mod, S, exec, hyprctl keyword bind ", Escape, exec, hyprctl keyword unbind ,Escape"''
-        "$mod, S, exec, eww open shutdown"
+        ''$mod, S, exec, ${pkgs.hyprland}/bin/hyprctl keyword bind ", Escape, exec, eww close shutdown"''
+        ''$mod, S, exec, ${pkgs.hyprland}/bin/hyprctl keyword bind ", Escape, exec, hyprctl keyword unbind ,Escape"''
+        "$mod, S, exec, ${pkgs.eww-wayland}/bin/eww open shutdown"
         # Screenshot keybind
         '',Print,exec,${pkgs.grim}/bin/grim -c -g "$(${pkgs.slurp}/bin/slurp)" - | ${pkgs.swappy}/bin/swappy -f -''
         # Binds to move between workspaces
-        "CTRL ALT, left, workspace, e-1"
-        "CTRL ALT, right, workspace, e+1"
-        "CTRL ALT SHIFT, left, movetoworkspace, e-1"
-        "CTRL ALT SHIFT, right, movetoworkspace, e+1"
 
         # Start rofi app launcher
         "$mod, D, exec, ${pkgs.rofi-wayland}/bin/rofi -show drun -show-icons"
@@ -53,14 +48,6 @@
         # Start alacritty
         "$mod, T, exec, ${pkgs.alacritty}/bin/alacritty"
 
-        # Close app
-        "$mod, C, killactive"
-
-        # Move focus keybinds
-        "$mod, left, movefocus, l"
-        "$mod, right, movefocus, r"
-        "$mod, up, movefocus, u"
-        "$mod, down, movefocus, d"
       ] ++ (
         # Workspace keybind
         # $mod + {1..10} to workspace {1..10}
@@ -73,5 +60,29 @@
             "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
           ]) 10));
     };
+    extraConfig = ''
+        bind=CTRL ALT, left, workspace, e-1
+        bind=CTRL ALT, right, workspace, e+1
+        bind=CTRL ALT SHIFT, left, movetoworkspace, e-1
+        bind=CTRL ALT SHIFT, right, movetoworkspace, e+1
+
+        
+        # Close app
+        bind=$mod, C, killactive
+
+        # Move focus keybinds
+        bind=$mod, left, movefocus, l
+        bind=$mod, right, movefocus, r
+        bind=$mod, up, movefocus, u
+        bind=$mod, down, movefocus, d
+
+        # Mouse bindings
+        bind=$mod, M, submap, mouse
+        submap=mouse
+        bindm=$mod,mouse:272,movewindow
+        bindm=$mod,mouse:273,resizewindow
+        bind=$mod, M, submap, reset
+        submap=reset
+    '';
   };
 }
