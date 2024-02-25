faa17269904ea048d702d1dc62abe6d84fdd20d0
Tiago Dinis <tiagodinis33@proton.me>
2024-02-25 22:53:34 +0000

    stylix: disable stylix for vscode and zellij

--- homes/x86_64-linux/livecd@live-iso-image/default.nix
+++ homes/x86_64-linux/livecd@live-iso-image/default.nix
@@ -2,4 +2,6 @@
 {
   home.stateVersion = "23.11";
   imports = [inputs.spicetify-nix.homeManagerModules.default];
+  stylix.targets.vscode.enable = false;
+  stylix.targets.zellij.enable = false;
 }
