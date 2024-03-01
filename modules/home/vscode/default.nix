d3819de47c29156ff0e92456dacc477196561eee
Tiago Dinis <tiagodinis33@proton.me>
2024-03-01 13:44:56 +0000

    vscode: customize discord rich presence

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -3,8 +3,45 @@
     enable = true;
     mutableExtensionsDir = false;
     userSettings = {
-      "workbench.colorTheme" = "Gruvbox Dark Hard";
       "git.confirmSync" = false;
+      "workbench.colorTheme" = "Gruvbox Dark Hard";
+      "vscord.status.problems.text" = "{problems_count} erros";
+  
+      "vscord.status.details.text.debugging" = "Deu merda na {workspace} e tou a debugar";
+      "vscord.status.state.text.debugging" = "A debugar o arquivo {file_name}{file_extension}";
+  
+      "vscord.status.details.text.editing" = "A mexer no {file_name}{file_extension}:{current_line}:{current_column}";
+      "vscord.status.state.text.editing" = "No projeto {workspace} (com {problems})";
+  
+      "vscord.status.details.text.notInFile" = "Não tou a editar nada";
+      "vscord.status.state.text.notInFile" = "Lembrei-me de procrastinar mais um pouco...";
+  
+      "vscord.status.details.text.idle" = "AFK";
+      "vscord.status.state.text.idle" = "Fui fazer outra cena mas esqueci o vscode aberto...";
+
+      "vscord.status.details.text.viewing" = "A mexer no arquivo {file_name}{file_extension}";
+      "vscord.status.state.text.viewing" = "No projeto {workspace} (com {problems})";
+  
+      "vscord.status.state.text.noWorkspaceFound" = "A usar o vscode só pra editar um arquivo...";
+      "vscord.status.details.text.noWorkSpaceText" = "A usar uma espada pra cortar um pão";
+
+      "vscord.status.image.large.viewing.text" = "A ver código {LANG}";
+      "vscord.status.image.small.viewing.text" = "Visual Studio Code";
+
+      "vscord.status.image.large.editing.text" = "A mexer com {LANG}";
+      "vscord.status.image.small.editing.text" = "Visual Studio Code";
+
+
+      "vscord.status.image.small.notInFile.text" = "Visual Studio Code";
+      "vscord.status.image.small.notInFile.key" = "https://raw.githubusercontent.com/LeonardSSH/vscord/main/assets/icons/vscode.png";
+
+      "vscord.status.image.large.notInFile.text" = "A procrastinar provavelmente...";
+      "vscord.status.image.large.notInFile.key" = "https://i.fbcd.co/products/original/454-97eae28d51681f0d1ffd5fcf220989abd5367188fc57b26d5b7d69da5b46252b.jpg";
+
+      "vscord.status.image.large.idle.text" = "AFK";
+      "vscord.status.image.large.idle.key" = "https://raw.githubusercontent.com/LeonardSSH/vscord/main/assets/icons/idle.png";
+      "vscord.status.idle.timeout" = 300;
+      "vscord.status.idle.resetElapsedTime" = true;
     };
     extensions = with pkgs.vscode-extensions;
       [
@@ -19,6 +56,7 @@
         llvm-vs-code-extensions.vscode-clangd
         jdinhlife.gruvbox
         editorconfig.editorconfig
+        tamasfe.even-better-toml
       ]
       ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
