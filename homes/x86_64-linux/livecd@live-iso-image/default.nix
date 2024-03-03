b626b58d232907be3f4d08267cce642895090b83
Tiago Dinis <tiagodinis33@proton.me>
2024-03-03 19:37:09 +0000

    cleanup: remove iso image

--- homes/x86_64-linux/livecd@live-iso-image/default.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, lib, inputs, ... }:
-{
-  home.stateVersion = "23.11";
-  imports = [inputs.spicetify-nix.homeManagerModules.default];
-  stylix.targets.vscode.enable = false;
-  stylix.targets.zellij.enable = false;
-}
