06e23d37f8545ce3ca6542d4c71e626008a443de
Tiago Dinis <tiagodinis33@proton.me>
2024-07-18 17:47:34 +0100

    nushell: remove welcome banner

--- modules/home/nushell/default.nix
+++ modules/home/nushell/default.nix
@@ -2,6 +2,9 @@
   programs.nushell = {
     shellAliases = {lg = "lazygit";};
     envFile.source = ./env.nu;
+    configFile.text = ''
+      $env.config.show_banner = false;
+    '';
   };
   programs.starship = {
     enable = true;
