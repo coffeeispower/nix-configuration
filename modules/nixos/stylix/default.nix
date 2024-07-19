a3efec299917328c65d0f6be33059f78bd281778
Tiago Dinis <tiagodinis33@proton.me>
2024-07-19 23:19:36 +0100

    stylix: change color scheme

--- modules/nixos/stylix/default.nix
+++ modules/nixos/stylix/default.nix
@@ -5,42 +5,15 @@
     name = "catppuccin-frappe-mauve-cursors";
     size = 8;
   };
-  stylix.base16Scheme = {
-    scheme = "Catppuccin Macchiato";
-    author = "https://github.com/catppuccin/catppuccin";
-    # #24273a
-    base00 = "24273a"; # base
-    # #1e2030
-    base01 = "1e2030"; # mantle
-    # #363a4f
-    base02 = "363a4f"; # surface0
-    # #494d64
-    base03 = "494d64"; # surface1
-    # #5b6078
-    base04 = "5b6078"; # surface2
-    # #cad3f5
-    base05 = "cad3f5"; # text
-    # #f4dbd6
-    base06 = "f4dbd6"; # rosewater
-    # #b7bdf8
-    base07 = "b7bdf8"; # lavender
-    # #ed8796
-    base08 = "ed8796"; # red
-    # #f5a97f
-    base09 = "f5a97f"; # peach
-    # #eed49f
-    base0A = "eed49f"; # yellow
-    # #a6da95
-    base0B = "a6da95"; # green
-    # #8bd5ca
-    base0C = "8bd5ca"; # teal
-    # #8aadf4
-    base0D = "8aadf4"; # blue
-    # #c6a0f6
-    base0E = "c6a0f6"; # mauve
-    # #f0c6c6
-    base0F = "f0c6c6"; # flamingo
-  };
+  stylix.base16Scheme = 
+    let
+      jsonOutputDrv =
+        pkgs.runCommand
+          "from-yaml"
+          { nativeBuildInputs = [ pkgs.remarshal ]; }
+          "remarshal -if yaml -i \"${./colorscheme.yaml}\" -of json -o \"$out\"";
+    in
+    builtins.fromJSON (builtins.readFile jsonOutputDrv);
   stylix.polarity = "dark";
   stylix.fonts = rec {
     monospace = {
